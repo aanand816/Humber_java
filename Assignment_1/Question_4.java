68fe1fcfe4f99675b6e51b0cbe4f9765f2dabe11
Aanand <aanandsaini968@gmail.com>
2024-10-05 22:49:56 -0400

    correct the code

--- Assignment_1/Question_4.java
+++ Assignment_1/Question_4.java
@@ -6,19 +6,23 @@ public class Question_4 {
     public static void main(String[] args) {
         //initializing the scanner class
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the numbers of line you want : ");
+        System.out.print("Enter the number from 1 to 15 : ");
         int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < (n-i); j++) {
-                System.out.print("  ");
+        if (n <= 15) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 0; j < (n - i); j++) {
+                    System.out.print("  ");
+                }
+                for (int j = i; j >= 1; j--) {
+                    System.out.print(j + " ");
+                }
+                for (int j = 2; j <= i; j++) {
+                    System.out.print(j + " ");
+                }
+                System.out.println();
             }
-            for (int j = i; j >=1 ; j--) {
-                System.out.print(j+" ");
-            }
-            for (int j=2; j<=i;j++){
-                System.out.print(j+" ");
-            }
-            System.out.println();
+        }else{
+            System.out.println("Please enter the number from 1 to 15 :)");
         }
     }
 }
